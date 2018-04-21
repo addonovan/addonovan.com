17da155eb74a37ab25a195efd148ac31bbb614f1
addonovan <austin@addonovan.com>
2018-04-20 23:07:33 -0500

    Created Configuration struct
    This can be used to do slightly different things while the
    server is in debug mode.

---
+++ src/config.rs
@@ -0,0 +1,18 @@
+
+pub struct Configuration
+{
+    pub address: &'static str,
+    pub debug: bool,
+}
+
+#[cfg(debug_assertions)]
+pub const CONFIG: Configuration = Configuration {
+    address: "127.0.0.1:8080",
+    debug: true
+};
+
+#[cfg(not(debug_assertions))]
+pub const CONFIG: Configuration = Configuration {
+    address: "0.0.0.0:80",
+    debug: false,
+};
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,16 @@
 extern crate mwf;
 
-mod projects;
-
-use projects::ProjectController;
-
 use mwf::ServerBuilder;
 
-#[cfg(debug_assertions)]
-const ADDR: &'static str = "127.0.0.1:8080";
-
-#[cfg(not(debug_assertions))]
-const ADDR: &'static str = "0.0.0.0:80";
+mod projects;
+mod config;
+use projects::ProjectController;
+use config::CONFIG;
 
 fn main()
 {
     ServerBuilder::new()
         .bind("/projects/:page?", ProjectController::new())
-        .addr(ADDR.parse().unwrap())
+        .addr(CONFIG.address.parse().unwrap())
         .start();
 }
