f2295fb797b5a6a4ca740cf61c0bd8875ebddc5a
addonovan <austin@addonovan.com>
2018-08-02 23:30:01 -0500

    map all requests to the requested file

--- src/main/kotlin/com/addonovan/website/MainController.kt
+++ src/main/kotlin/com/addonovan/website/MainController.kt
@@ -6,14 +6,40 @@ import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.servlet.HandlerMapping
+import javax.servlet.http.HttpServletRequest
 
 @Controller
 class MainController {
 
-    @GetMapping("")
-    fun index(model: Model): String {
+    // holy fucking shit, why is this variable so damn long?
+    // like, this is absolutely ridiculous
+    private val PATH_ATTR =
+            HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE
+
+    @GetMapping("**")
+    fun get(
+            request: HttpServletRequest,
+            model: Model
+    ): String {
+        val file = (request.getAttribute(PATH_ATTR) as String).let {
+            val file = it.trimStart('/')
+
+            if (file.isEmpty()) {
+                "index.html"
+            } else {
+                "$file.html"
+            }
+        }
+
         model["style"] = FileCache["style.css"]
-        model["content"] = FileCache["index.html"]
+        model["content"] = try {
+            FileCache[file]
+        }
+        catch (e: Exception) {
+            FileCache["404.html"]
+        }
+
         return "format"
     }
 
