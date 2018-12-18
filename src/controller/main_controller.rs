bf26c05847f0662b0bc32b5fb8e676f849cbbd3b
addonovan <austin@addonovan.com>
2018-12-17 18:13:28 -0600

    create get_server_ip to get server's ip

--- src/controller/main_controller.rs
+++ src/controller/main_controller.rs
@@ -41,6 +41,34 @@ impl MainController {
             .finish()
     }
 
+    pub fn get_server_ip(&self) -> Result<String> {
+        use std::process::Command;
+        let instance_info = Command::new("gcloud")
+            .arg("compute")
+            .arg("instances")
+            .arg("list")
+            .arg("--filter=game-server")
+            .arg("--format=config")
+            .output()?
+            .stdout;
+
+        let natip_line = String::from_utf8(instance_info)?
+            .lines()
+            .filter(|line| line.starts_with("natIP"))
+            .take(1)
+            .collect::<Vec<&str>>()
+            .pop()
+            .ok_or("Game server is not running".into())?;
+
+        let ip = natip_line.split(' ')
+            .skip(2)
+            .take(1)
+            .next()
+            .expect("Illegal data format from gcloud!");
+
+        Ok(ip.to_owned())
+    }
+
 }
 
 impl Controller for MainController {
